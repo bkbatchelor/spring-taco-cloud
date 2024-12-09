523b07a406bfe596b44c98626f0fd38baa30d92d
bkbatchelor <brian.batchelor@sandboxdev.io>
2024-12-09 11:12:18 -0500

    FEAT: Replace Lombok @Data with Java Record

--- src/main/java/io/sandboxdev/taco/Ingredient.java
+++ src/main/java/io/sandboxdev/taco/Ingredient.java
@@ -1,8 +1,5 @@
 package io.sandboxdev.taco;
 
-import lombok.Data;
-
-
 public record Ingredient(String id, String name, io.sandboxdev.taco.Ingredient.Type type) {
     public static enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
