304119cb1181a154628d31e72f94b317ce7d5512
bkbatchelor <brian.batchelor@sandboxdev.io>
2024-12-10 17:23:03 -0500

    FEAT: Add POST method to process taco order

--- src/main/java/io/sandboxdev/taco/web/OrderController.java
+++ src/main/java/io/sandboxdev/taco/web/OrderController.java
@@ -1,10 +1,13 @@
 package io.sandboxdev.taco.web;
 
+import io.sandboxdev.taco.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 @Slf4j
 @Controller
@@ -14,5 +17,12 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 }
